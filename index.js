6c4c76cd8c4ccd0153b3e69f550d1fd91d6d7ff2
Vicky Ahuja <vickyahuja371@gmail.com>
2023-02-04 19:38:01 +0530

    run static HTML files

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
-    const name = req.query.name
-    res.send(`
-        <h1>Welcome, ${name ?? 'User'}</h1>
-        <a href="/about">Go to about page</a>
-    `)
-})
+const path = require('path')
 
-app.get('/about', (req, res) => {
-    const name = req.query.name
-    res.send(`
-        <h1>About us</h1>
-        <input type="text" placeholder="Enter name" value="${name ?? ''}" />
-        <a href="/">Go to Home</a>
-    `)
-})
+const publicPath = path.join(__dirname, 'public')
 
-app.get('/help', (req, res) => {
-    res.send('Welcome to help page')
-})
+app.use(express.static(publicPath))
+
+// app.get('', (req, res) => {
+//     const name = req.query.name
+//     res.send(`
+//         <h1>Welcome, ${name ?? 'User'}</h1>
+//         <a href="/about">Go to about page</a>
+//     `)
+// })
+
+// app.get('/about', (req, res) => {
+//     const name = req.query.name
+//     res.send(`
+//         <h1>About us</h1>
+//         <input type="text" placeholder="Enter name" value="${name ?? ''}" />
+//         <a href="/">Go to Home</a>
+//     `)
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send('Welcome to help page')
+// })
 
 app.listen(5000)
