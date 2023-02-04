bfd6e39c44e910034ff521d44ec1174677706f26
Vicky Ahuja <vickyahuja371@gmail.com>
2023-02-04 20:08:43 +0530

    render dynamic pages with ejs

---
+++ 6_render_html_files_wtot_ext_and_404_page.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+const publicPath = path.join(__dirname, 'public')
+
+/**
+ * using below code we dont need to write file extension to access route
+ */
+
+app.get('', (_, res) => {
+    res.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (_, res) => {
+    res.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/help', (_, res) => {
+    res.sendFile(`${publicPath}/help.html`)
+})
+
+app.get('*', (_, res) => {
+    res.sendFile(`${publicPath}/nopage.html`)
+})
+
+app.listen(5000)
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path')
+const ejs = require('ejs')
+
 const app = express();
 
-const path = require('path')
+app.set('view engine', ejs);
 
 const publicPath = path.join(__dirname, 'public')
-
-app.use(express.static(publicPath))
+const viewsPath = path.join(__dirname, 'views')
 
 /**
  * using below code we dont need to write file extension to access route
@@ -15,16 +17,27 @@ app.get('', (_, res) => {
     res.sendFile(`${publicPath}/index.html`)
 })
 
-app.get('/about', (_, res) => {
-    res.sendFile(`${publicPath}/about.html`)
+app.get('/profile', (_, res) => {
+    const user = {
+        name: 'Vicky Ahuja',
+        email: 'vicky@test.com',
+        mobile: '0099887766'
+    }
+    res.render(`${viewsPath}/profile.ejs`, {
+        user
+    })
 })
 
-app.get('/help', (_, res) => {
-    res.sendFile(`${publicPath}/help.html`)
-})
+// app.get('/about', (_, res) => {
+//     res.sendFile(`${publicPath}/about.html`)
+// })
 
-app.get('*', (_, res) => {
-    res.sendFile(`${publicPath}/nopage.html`)
-})
+// app.get('/help', (_, res) => {
+//     res.sendFile(`${publicPath}/help.html`)
+// })
+
+// app.get('*', (_, res) => {
+//     res.sendFile(`${publicPath}/nopage.html`)
+// })
 
 app.listen(5000)
