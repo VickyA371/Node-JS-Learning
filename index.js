9cda84aa86fae1d028828b22b877669ad392b9f4
Vicky Ahuja <vickyahuja371@gmail.com>
2023-02-04 15:06:48 +0530

    navigating between routes

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-    res.send('Welcome to home page')
+    const name = req.query.name
+    res.send(`
+        <h1>Welcome, ${name ?? 'User'}</h1>
+        <a href="/about">Go to about page</a>
+    `)
 })
 
 app.get('/about', (req, res) => {
-    res.send('Welcome to about page')
+    const name = req.query.name
+    res.send(`
+        <h1>About us</h1>
+        <input type="text" placeholder="Enter name" value="${name ?? ''}" />
+        <a href="/">Go to Home</a>
+    `)
 })
 
 app.get('/help', (req, res) => {
